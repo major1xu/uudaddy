bcc8e25cd62060fbed977310c1a5200b7e4c5465
Minjie Xu <Minjie.Xu@gmail.com>
2018-12-05 15:00:17 -0600

    make base case work for min diff

--- MaxMinSumsDiffs.java
+++ MaxMinSumsDiffs.java
@@ -30,11 +30,13 @@ public class MaxMinSumsDiffs {
         int numberOfArgs = args.length;
         System.out.println("numberOfArgs=" + numberOfArgs);
         ArrayList<Integer> remainingIntegers = new ArrayList<Integer>(numberOfArgs);
+        ArrayList<Integer> remainingIntegersCopy = new ArrayList<Integer>(numberOfArgs);
+
         //Digit[] remainingDigits = new Digit[numberOfArgs];
 
         for(int ii=0; ii<numberOfArgs;ii++) {
             checkerBoard.add(Integer.parseInt( args[ii]) );
-            System.out.println("Integer.parseInt( args[ii])=" + Integer.parseInt( args[ii]));
+            //System.out.println("Integer.parseInt( args[ii])=" + Integer.parseInt( args[ii]));
             remainingIntegers.add(Integer.parseInt( args[ii]));
             //remainingDigits[ii]=new Digit(Integer.parseInt(args[ii]));
         }
@@ -44,19 +46,7 @@ public class MaxMinSumsDiffs {
 
         // https://stackoverflow.com/questions/14475556/how-to-sort-arraylist-of-objects
         // descending order  - better way
-        Collections.sort(remainingIntegers, new Comparator<Integer>() {
-            @Override public int compare(Integer p1, Integer p2) {
-                int returnValue = 0;
-
-                if ((p1.intValue() - p2.intValue()) > 0) {
-                    return -1;
-                } else if ((p1.intValue() - p2.intValue()) == 0) {
-                    return 0;
-                } else {
-                    return 1;
-                }
-            }
-        });
+        Collections.sort(remainingIntegers, new IntegerComparator());
 
         // http://www.mkyong.com/java/java-object-sorting-example-comparable-and-comparator/
         // descending order
@@ -64,9 +54,10 @@ public class MaxMinSumsDiffs {
 
         System.out.println("After sorting:");
         for(int ii=0; ii<numberOfArgs;ii++) {
-            System.out.println("remainingIntegers[ii]=" + remainingIntegers.get(ii));
+            //System.out.println("remainingIntegers[ii]=" + remainingIntegers.get(ii));
             //System.out.println("remainingDigits[ii]=" + remainingDigits[ii].getQuantity());
         }
+        System.out.println("remainingIntegers.size()=" + remainingIntegers.size());
 
         //
         // compose the rest of numbers so that the big number gets small numbers, and put them in small to largest
@@ -82,23 +73,97 @@ public class MaxMinSumsDiffs {
         int number_1 = 0;
         int number_2 = 0;
         int difference = 0;
-        int number_1_thousand;
-        int number_1_hundred;
+        int number_1_thousand=0;
+        int number_2_thousand=0;
+        int number_1_hundred=0;
+        int number_2_hundred=0;
+        int number_1_tenth=0;
+        int number_2_tenth=0;
+        int number_1_single=0;
+        int number_2_single=0;
+        boolean found_min_diff=false;
+        int min_diff=0;
 
         int ii=0;
-        for(ii=0; ii<numberOfArgs; ii++) {
-            int intNum = remainingIntegers.get(ii);
-            if( intNum != 0) {
-                number_1_thousand = intNum;
-                remainingIntegers.remove(intNum);
+        while(remainingIntegers.isEmpty()==false && found_min_diff==false) {
+            Integer intNum = remainingIntegers.get(ii);
+            number_1_thousand = intNum;
+            //System.out.println("number_1_thousand=" + number_1_thousand);
+            remainingIntegers.remove(new Integer(intNum));
 
-                if( remainingIntegers.isEmpty() == false ) {
-                    number_1_hundred = remainingIntegers.get(0);  // use remainingIntegers?
-                    remainingIntegers.remove(args[ii]);
-                }
-
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                intNum = remainingIntegers.get(ii);
+                number_2_thousand = intNum;
+                //System.out.println("number_2_thousand=" + number_2_thousand);
+                remainingIntegers.remove(new Integer(intNum));
             }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                Collections.sort(remainingIntegers);
+
+                intNum = remainingIntegers.get(ii);
+                number_1_hundred = intNum;
+                //System.out.println("number_1_hundred=" + number_1_hundred);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                intNum = remainingIntegers.get(ii);
+                number_1_tenth = intNum;
+                //System.out.println("number_1_tenth=" + number_1_tenth);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                intNum = remainingIntegers.get(ii);
+                number_1_single = intNum;
+                //System.out.println("number_1_single=" + number_1_single);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                Collections.sort(remainingIntegers, new IntegerComparator());
+
+                intNum = remainingIntegers.get(ii);
+                number_2_hundred = intNum;
+                //System.out.println("number_2_hundred=" + number_2_hundred);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                intNum = remainingIntegers.get(ii);
+                number_2_tenth = intNum;
+                //System.out.println("number_2_tenth=" + number_2_tenth);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            if( remainingIntegers.isEmpty() == false && found_min_diff==false) {
+                intNum = remainingIntegers.get(ii);
+                number_2_single = intNum;
+                //System.out.println("number_2_single=" + number_2_single);
+                remainingIntegers.remove(new Integer(intNum));
+            }
+
+            int number1=number_1_thousand*1000+number_1_hundred*100+number_1_tenth*10+number_1_single;
+            int number2=number_2_thousand*1000+number_2_hundred*100+number_2_tenth*10+number_2_single;
+            min_diff=number1-number2;
+            System.out.println("number1=" + number1);
+            System.out.println("number2=" + number2);
+            System.out.println("min_diff=" + min_diff);
         }
     }
 
+    private static class IntegerComparator implements Comparator<Integer> {
+        @Override public int compare(Integer p1, Integer p2) {
+            int returnValue = 0;
+
+            if ((p1.intValue() - p2.intValue()) > 0) {
+                return -1;
+            } else if ((p1.intValue() - p2.intValue()) == 0) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
 }
